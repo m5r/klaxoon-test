37a5c36989a4ae2ea305212d33bf38bac340b9d1
m5r <mokht@rmi.al>
2021-03-25 16:08:37 +0100

    style edit page

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -4,13 +4,15 @@ import TagInput from "@pathofdev/react-tag-input";
 
 import useBookmark from "../hooks/use-bookmark";
 
+import DeleteIcon from "../components/delete-icon";
+
 type Params = {
 	bookmarkId: string;
 }
 
 const EditPage: FunctionComponent = () => {
 	const { bookmarkId } = useParams<Params>();
-	const { bookmark, updateKeywords } = useBookmark(bookmarkId);
+	const { bookmark, updateKeywords, deleteBookmark } = useBookmark(bookmarkId);
 
 	if (!bookmark) {
 		return (
@@ -22,6 +24,23 @@ const EditPage: FunctionComponent = () => {
 
 	return (
 		<div>
+			<div className="py-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
+				<h3 className="text-lg leading-6 font-medium text-gray-900">
+					Editing <i>{bookmark.title}</i>
+				</h3>
+				<div className="mt-3 flex sm:mt-0 sm:ml-4">
+					<button
+						onClick={() => deleteBookmark()}
+						type="button"
+						style={{ lineHeight: 1 }}
+						className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto"
+					>
+						<DeleteIcon />
+						<span className="ml-2">Delete</span>
+					</button>
+				</div>
+			</div>
+
 			<div className="ml-4 mt-4">
 				<div className="flex items-center">
 					<div className="flex-shrink-0">
