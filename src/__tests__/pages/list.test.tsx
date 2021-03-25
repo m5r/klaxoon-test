a8d3bbd72a51fa811b362b84c12d2bbc999012bd
m5r <mokht@rmi.al>
2021-03-25 17:22:06 +0100

    display video durations

--- src/__tests__/pages/list.test.tsx
+++ src/__tests__/pages/list.test.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
+import localForage from "localforage";
 
 import ListPage from "../../pages/list";
 import { BookmarksProvider } from "../../contexts/bookmarks-context";
+import type Bookmark from "../../models/bookmark";
+
+jest.mock("localforage", () => ({
+	__esModule: true,
+	default: ({
+		getItem: jest.fn(),
+	}),
+}));
 
 describe("/list page", () => {
+	const mockedGetItem = localForage.getItem as any as jest.Mock<Promise<Bookmark[]>>;
+	mockedGetItem.mockResolvedValue([]);
+
 	const wrapper = BookmarksProvider;
 
 	test("renders list of bookmarks", async () => {
+		// test keeps failing because of asynchronous stuff happening in <BookmarksProvider />
+
 		render(<ListPage />, { wrapper });
-		const bookmarkCountElement = screen.getByText("You have 0 bookmarks");
-		await waitFor(() => expect(bookmarkCountElement).toBeInTheDocument());
+		expect(screen.getByText("Loading your bookmarks")).toBeInTheDocument()
+		await waitForElementToBeRemoved(screen.getByText("Loading your bookmarks"));
+		await waitFor(() => expect(screen.getByText("You have 0 bookmarks")).toBeInTheDocument());
 	});
 });
